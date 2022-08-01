3125a2f2266a1fff3f0595df3db2728856185454
erennilufer <eren.nilufer@outlook.com>
2022-08-01 18:56:03 +0300

    revised components

--- src/components/AirportList/AirportList.js
+++ src/components/AirportList/AirportList.js
@@ -2,15 +2,16 @@ import { View, Text, TouchableHighlight } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { FlatList } from 'react-native-gesture-handler'
-
+import AirportCard from '../AirportCard/AirportCard'
 const AirportList = () => {
     const airports=useSelector((state)=>state.airport.airports)
   return (
     <FlatList 
-    
+    style={{flexDirection:"column",width:"100%"}}
+     
     data={airports}
     renderItem={({ item }) => (
-     <Text>{console.log(item)}</Text>
+     <AirportCard item={"item"}/>
     )}
   />
   )
