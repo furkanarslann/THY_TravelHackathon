7e0244cfa4fc93096a936d2248f9b207b603230d
Furkan ARSLAN <furkanarslan.edu@outlook.com>
2022-08-02 00:11:59 +0300

    carousel added, needs a fix for dots

--- src/components/Flights/Flights.js
+++ src/components/Flights/Flights.js
@@ -3,7 +3,6 @@ import React from "react";
 import { FlatList } from "react-native-gesture-handler";
 import FlightCard from "../FlightCard/FlightCard";
 import { useSelector } from "react-redux";
-import SelectDropdown from "react-native-select-dropdown";
 
 const Flights = () => {
   const flights = useSelector((state) => state.flight.flights);
@@ -24,25 +23,7 @@ const Flights = () => {
           Uçuş Bul
         </Text>
       </View>
-      <View>
-        <SelectDropdown
-        
-        data={countries}
-          onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index);
-          }}
-          buttonTextAfterSelection={(selectedItem, index) => {
-            // text represented after item is selected
-            // if data array is an array of objects then return selectedItem.property to render after item is selected
-            return selectedItem;
-          }}
-          rowTextForSelection={(item, index) => {
-            // text represented for each item in dropdown
-            // if data array is an array of objects then return item.property to represent item in dropdown
-            return item;
-          }}
-        />
-      </View>
+      
       <FlatList
         numColumns={2}
         data={flights}
