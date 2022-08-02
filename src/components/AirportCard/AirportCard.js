214133ec2237033ed8ebe837e031f60a81cba484
erennilufer <eren.nilufer@outlook.com>
2022-08-02 10:12:43 +0300

    revised components

--- src/components/AirportCard/AirportCard.js
+++ src/components/AirportCard/AirportCard.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from './AirportCard.style'
-const AirportCard = ({item}) => {
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import styles from "./AirportCard.style";
+const AirportCard = ({ item }) => {
+  let ct = 0;
+  const [city, setCity] = useState("");
+
+  //longer
+  useEffect(() => {
+    console.log(item.LanguageInfo.Language);
+    Array.isArray(item.City.PortsInCity.Port)
+      ? setCity(item.City.PortsInCity.Port[0].Code)
+      : setCity(item.City.PortsInCity.Port.Code);
+    /*       Array.isArray(item.City.LanguageInfo.Language)?setCity(item.City.LanguageInfo.Language[0].Name): item.City.LanguageInfo.Language?setCity(item.City.LanguageInfo.Language.Name):setCity("error")
+     */
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>asdas</Text>
+      <Text>{city}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default AirportCard
+export default AirportCard;
