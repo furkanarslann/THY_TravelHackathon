9316e731c9f6f4b08df2e1da4861409e5f17428c
erennilufer <eren.nilufer@outlook.com>
2022-08-02 16:21:42 +0300

    revised airport components

--- src/components/AirportList/AirportList.js
+++ src/components/AirportList/AirportList.js
@@ -1,27 +1,39 @@
-import { View, Text, TouchableHighlight } from 'react-native'
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { FlatList, ScrollView } from 'react-native-gesture-handler'
-import AirportCard from '../AirportCard/AirportCard'
+import { View, Text, TouchableHighlight } from "react-native";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
+import AirportCard from "../AirportCard/AirportCard";
+import { setTags } from "../../redux/slices/airportSlice";
 const AirportList = () => {
-    const airports=useSelector((state)=>state.airport.airports)
-/* useEffect(() => {
+  const airports = useSelector((state) => state.airport.airports);
+  const tags = useSelector((state) => state.airport.tags);
+  const dispatch=useDispatch();
+
+  /* useEffect(() => {
   airports.forEach((item)=>{
     console.log(item.City.LanguageInfo.Language[0].Name);
   })
 }, []) */
-   return (
- 
+  useEffect(() => {
+    const arr = airports.map((item) => {
+      return Array.isArray(item.City.PortsInCity.Port)
+        ? item.City.PortsInCity.Port[0].Code
+        : item.City.PortsInCity.Port.Code;
+    });
+    dispatch(setTags(arr.sort()));
+
+    
+  }, []);
+
+  return (
     <FlatList
       numColumns={2}
       data={airports}
       renderItem={({ item, index }) => {
-       return  <AirportCard item={item}/>
-      }
-      }
+        return <AirportCard item={item} />;
+      }}
     />
- 
-   )
-}
+  );
+};
 
-export default AirportList
+export default AirportList;
