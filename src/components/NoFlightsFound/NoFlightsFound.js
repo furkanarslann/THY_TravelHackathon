5ac739b62a3304a63f65fb6fe3c5bfc07198e8a9
erennilufer <eren.nilufer@outlook.com>
2022-08-03 16:57:45 +0300

    added noFlightFound component

---
+++ src/components/NoFlightsFound/NoFlightsFound.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NoFlightsFound = () => {
+  return (
+    <View style={{justifyContent:"center",alignItems:"center",flex:1,backgroundColor:"#ffffff"}}>
+      <Text>Search Flight</Text>
+    </View>
+  )
+}
+
+export default NoFlightsFound
